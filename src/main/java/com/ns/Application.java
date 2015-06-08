c99e4e2063b7c42da22a20911bc47709a1be3130
Qualitative <qualitative1@gmail.com>
2015-06-08 11:44:26 +0300

    Initial commit

---
+++ src/main/java/com/ns/Application.java
@@ -0,0 +1,13 @@
+package com.ns;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class Application {
+
+    private static final Logger LOG = LoggerFactory.getLogger(Application.class);
+
+    public static void main(String[] args) {
+        LOG.debug("Start application");
+    }
+}
